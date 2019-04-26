ea4121b4126ba3975ec0799e2d41a871acd9bc4c
Ao Li <aoli1996@berkeley.edu>
2019-04-26 13:19:23 -0500

    Apr26-1319

--- cmd/coordinator/main.go
+++ cmd/coordinator/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+var (
+	serverPorts = []string{"7001", "7002", "7003", "7004", "7005"}
+)
+
+func startClient() {
+
+}
+
+func startServer() {
+
+}
+
 func main(){
 	fmt.Println("This is the coordinator code")
 }
