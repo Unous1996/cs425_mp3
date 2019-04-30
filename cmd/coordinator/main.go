de647b61bad929115dbf2800f5321fa2042047cc
Ao Li <aoli1996@berkeley.edu>
2019-04-29 21:39:36 -0500

    Apr29-2139

--- cmd/coordinator/main.go
+++ cmd/coordinator/main.go
@@ -7,7 +7,6 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-	"time"
 )
 
 type Node struct {
@@ -33,6 +32,10 @@ var (
 	workingChan chan bool
 	dialChan chan bool
 	abortChans [10]chan bool
+	beginChans [10]chan bool
+	commitChans [10]chan bool
+	setChans [10]chan string
+	getChans [10]chan string
 )
 
 var (
@@ -117,6 +120,7 @@ func startCoordinator() {
 		if err != nil {
 			fmt.Printf("#Failed to connect to Server%v.", serverName[i])
 		}
+
 		serverConnMap[serverName[i]] = conn
 		defer conn.Close()
 
@@ -127,8 +131,7 @@ func startCoordinator() {
 
 		conn, _ := tcpListen.AcceptTCP()
 		defer conn.Close()
-		go handleTransaction(conn)
-
+		go readMessage(conn)
 	}
 
 }
@@ -150,331 +153,437 @@ func initialize(){
 	mapInit()
 }
 
-func handleTransaction(conn net.Conn)  {
+func readMessage(conn net.Conn) {
 
 	buff := make([]byte, 10000)
 	endChan := make(chan bool)
-	count := 0
-	abort := false
-	//remoteIpIndex := ip2ChannelIndexMap[strings.Split(conn.RemoteAddr().String(),":")[1]]
+	remoteIpIndex := ip2ChannelIndexMap[strings.Split(conn.RemoteAddr().String(),":")[0]]
+	transactionPrefix := conn.RemoteAddr().String()
+	fmt.Println("remoteIpIndex = ", remoteIpIndex)
+	go handleTransaction(conn, remoteIpIndex, transactionPrefix)
 
 	for {
-
-		var transactionID string
-		for {
-
-			j, err := conn.Read(buff)
-			if err != nil {
-				fmt.Println("#Failed to connect to Client.")
-				endChan <- true
-			}
-
-			msg := string(buff[0:j])
-
-			if msg == "BEGIN" {
-
-				count += 1
-				transactionID = conn.RemoteAddr().String() + "_" + strconv.Itoa(count)
-				fmt.Println("#Start a transaction whose ID is", transactionID)
-				abort = false
-				break;
-			}
-
+		j, err := conn.Read(buff)
+		if err != nil {
+			fmt.Println("#Failed to connect to Client.")
+			endChan <- true
 		}
 
-		updateMap := make(map[string]string)
-		logMap := make(map[string][]string)
-		holdLockMap := make(map[string]int)
-
-		for {
-
-			if abort == true {
-				break
-			}
-
-			j, err := conn.Read(buff)
-			flag := checkErr(err)
-			if flag == 0 {
-				break
-			}
-			
-			line := string(buff[0:j])
-			lineSplit := strings.Split(line, " ")
-
-			if lineSplit[0] == "ABORT" {
-
-				for k, v := range holdLockMap {
-
-					if v == 1 {
-
-						lockMapMutex.Lock()
-						lockMap[k][1] -= 1
-						lockMapMutex.Unlock()
-						readLockHolderMNapMutex.Lock()
-						delete(readLockHolderMNap[k], transactionID)
-						readLockHolderMNapMutex.Unlock()
-					}
-
-					if v == 2 {
-						lockMapMutex.Lock()
-						lockMap[k][2] = 0
-						lockMapMutex.Unlock()
-						writeLockHolderMNapMutex.Lock()
-						writeLockHolderMNap[k] = ""
-						writeLockHolderMNapMutex.Unlock()
-					}
-
-				}
-
-				conn.Write([]byte("ABORTED"))
-				break
-			}
-
-			if lineSplit[0] == "COMMIT" {
-
-				for k, v := range logMap {
-
-					serverConnMapMutex.RLock()
-					serverConn := serverConnMap[k]
-					serverConnMapMutex.RUnlock()
-
-					for _, line := range v {
-						serverConn.Write([]byte(line+"\n"))
-					}
-				}
-
-				for k, v := range holdLockMap {
-
-					if v == 1 {
-
-						lockMapMutex.Lock()
-						lockMap[k][1] -= 1
-						lockMapMutex.Unlock()
-						readLockHolderMNapMutex.Lock()
-						delete(readLockHolderMNap[k], transactionID)
-						readLockHolderMNapMutex.Unlock()
-					}
-
-					if v == 2 {
-
-						lockMapMutex.Lock()
-						lockMap[k][2] = 0
-						lockMapMutex.Unlock()
-						writeLockHolderMNapMutex.Lock()
-						writeLockHolderMNap[k] = ""
-						writeLockHolderMNapMutex.Unlock()
-					}
-
-				}
-
-				conn.Write([]byte("COMMIT OK"))
-				break
-			}
-
-			if lineSplit[0] == "SET" {
-
-				server := strings.Split(lineSplit[1],".")[0]
-
-				for {
-
-					lockMapMutex.RLock()
-					_, ok := lockMap[lineSplit[1]]
-					lockMapMutex.RUnlock()
-
-					if !ok {
-
-						lockMapMutex.Lock()
-						lockMap[lineSplit[1]] = map[int]int{}
-						lockMapMutex.Unlock()
-
-					}
-
-					lockMapMutex.RLock()
-					WLock := lockMap[lineSplit[1]][2]
-					RLock := lockMap[lineSplit[1]][1]
-					lockMapMutex.RUnlock()
-
-					if WLock >= 1 {
-
-						if holdLockMap[lineSplit[1]] == 2 {
-
-							updateMap[lineSplit[1]] = lineSplit[2]
-							logMap[server] = append(logMap[server], line)
-							conn.Write([]byte("OK"))
-							break
-						} else  {
-							/*
-							select {
-								case <- abortChans[remoteIpIndex]:
-									fmt.Println("beginning to abort")
-							}
-							*/
-
-						}
-
-					} else {
-
-						if RLock == 0 || ( RLock == 1 && holdLockMap[lineSplit[1]] == 1) {
-
-							lockMapMutex.Lock()
-							lockMap[lineSplit[1]][2] = 1
-							lockMapMutex.Unlock()
-
-							writeLockHolderMNapMutex.Lock()
-							writeLockHolderMNap[lineSplit[1]] = transactionID
-							writeLockHolderMNapMutex.Unlock()
-
-							holdLockMap[lineSplit[1]] = 2
-
-							if RLock == 1 {
-
-								lockMapMutex.Lock()
-								lockMap[lineSplit[1]][1] -= 1
-								readLockHolderMNapMutex.Lock()
-								delete(readLockHolderMNap, lineSplit[1])
-								readLockHolderMNapMutex.Unlock()
-								lockMapMutex.Unlock()
-
-							}
-
-							updateMap[lineSplit[1]] = lineSplit[2]
-							logMap[server] = append(logMap[server], line)
-							conn.Write([]byte("OK"))
-							break
-						}
-					}
-
-				}
-
-				continue
-
-			}
-
-			if lineSplit[0] == "GET" {
-				fmt.Println("Received a GET")
-				for {
-
-					lockMapMutex.RLock()
-					_, ok := lockMap[lineSplit[1]]
-					lockMapMutex.RUnlock()
-
-					if !ok {
-
-						lockMapMutex.Lock()
-						lockMap[lineSplit[1]] = map[int]int{}
-						lockMapMutex.Unlock()
-
-					}
-
-					lockMapMutex.RLock()
-					WLock := lockMap[lineSplit[1]][2]
-					lockMapMutex.RUnlock()
-
-					if WLock == 1 && holdLockMap[lineSplit[1]] != 2 {
-						continue
-					}
-					fmt.Println("Reached here 1")
-					if holdLockMap[lineSplit[1]] == 0 {
-
-						holdLockMap[lineSplit[1]] = 1
-						lockMapMutex.Lock()
-						lockMap[lineSplit[1]][1] += 1
-
-						readLockHolderMNapMutex.RLock()
-						_, ok := readLockHolderMNap[lineSplit[1]]
-						readLockHolderMNapMutex.RUnlock()
-						readLockHolderMNapMutex.Lock()
-						if ! ok {
-							readLockHolderMNap[lineSplit[1]] = map[string]bool{}
-						}
-						readLockHolderMNap[lineSplit[1]][transactionID] = true
-						readLockHolderMNapMutex.Unlock()
-						lockMapMutex.Unlock()
-					}
-
-					break
-				}
-
-				fmt.Println("Reached here 2")
-				server := strings.Split(lineSplit[1],".")[0]
-				v, ok := updateMap[lineSplit[1]]
-				fmt.Println("Reached here 3")
-
-				if ok {
-
-					msg := lineSplit[1] + " = " + v
-					conn.Write([]byte(msg))
-
-				} else {
-
-					serverConnMapMutex.RLock()
-					serverConn := serverConnMap[server]
-					serverConnMapMutex.RUnlock()
-					serverConn.Write([]byte(line+"\n"))
-					fmt.Println("Send some bytes to the server")
-					j, err := serverConn.Read(buff)
-
-					if err != nil {
-						fmt.Printf("#Failed to connect to Server%v.", server)
-						panic("Server" + server + " disconnected.")
-					}
-
-					if string(buff[0:j]) == "NO" {
-						// return NOT FOUND and abort the transaction.
-						for k, v := range holdLockMap {
-							if v == 1 {
-
-								lockMapMutex.Lock()
-								lockMap[k][1] -= 1
-								lockMapMutex.Unlock()
-								readLockHolderMNapMutex.Lock()
-								delete(readLockHolderMNap[k], transactionID)
-								readLockHolderMNapMutex.Unlock()
-							}
-
-							if v == 2 {
-								lockMapMutex.Lock()
-								lockMap[k][2] = 0
-								lockMapMutex.Unlock()
-								writeLockHolderMNapMutex.Lock()
-								writeLockHolderMNap[k] = ""
-								writeLockHolderMNapMutex.Unlock()
-							}
-						}
-
-						conn.Write([]byte("NO FOUND"))
-						conn.Write([]byte("This transaction is being aborted, please start a new one."))
-
-						break
-
-					} else {
-
-						updateMap[lineSplit[1]] = string(buff[0:j])
-						msg := lineSplit[1] + " = " + string(buff[0:j])
-						conn.Write([]byte(msg))
-
-					}
-				}
-
-				continue
-
-			}
-
+		msg := string(buff[0:j])
+
+		if msg == "BEGIN" {
+			fmt.Println("recevide a BEGIN")
+			beginChans[remoteIpIndex] <- true
+			continue
 		}
 
-		/* Shall never break from this loop */
+		if msg == "COMMIT" {
+			commitChans[remoteIpIndex] <- true
+			continue
+		}
+
+		if msg == "ABORT" {
+			abortChans[remoteIpIndex] <- true
+			continue
+		}
+
+		messageSplit := strings.Split(msg, " ")
+		messageType := messageSplit[0]
+
+
+		if messageType == "SET" {
+			fmt.Println("received a SET")
+			setChans[remoteIpIndex] <- msg
+		}
+
+		if messageType == "GET" {
+			getChans[remoteIpIndex] <- msg
+		}
+
 	}
 
 	<-endChan
 }
 
-func sendAbortSignal(abortChan chan bool){
-	fmt.Println("Reached here")
-	for{
-		time.Sleep(1*time.Second)
-		abortChan <- true
-		fmt.Println("Sent an abort signal")
+func handleTransaction(conn net.Conn, remoteIpindex int, transactionPrefix string)  {
+
+	count := 0
+	serverBuff := make([]byte, 10000)
+	//remoteIpIndex := ip2ChannelIndexMap[strings.Split(conn.RemoteAddr().String(),":")[1]]
+
+	for {
+		fmt.Println("Waiting for BEGIN")
+		<-beginChans[remoteIpindex]
+		count += 1
+		transactionID := transactionPrefix + "_" + strconv.Itoa(count)
+		fmt.Println("#Start a transaction whose ID is", transactionID)
+		conn.Write([]byte("OK"))
+
+		updateMap := make(map[string]string)
+		logMap := make(map[string][]string)
+		holdLockMap := make(map[string]int)
+		breakLoop :=  false
+
+		for {
+			fmt.Printf("RemoteIdx = %d, breakLoop = %d\n", remoteIpindex, breakLoop)
+			if breakLoop {
+				break
+			}
+
+			select {
+				case <- commitChans[remoteIpindex]:
+					for k, v := range logMap {
+
+						serverConnMapMutex.RLock()
+						serverConn := serverConnMap[k]
+						serverConnMapMutex.RUnlock()
+
+						for _, line := range v {
+							serverConn.Write([]byte(line+"\n"))
+						}
+					}
+
+					for k, v := range holdLockMap {
+
+						if v == 1 {
+
+							lockMapMutex.Lock()
+							lockMap[k][1] -= 1
+							lockMapMutex.Unlock()
+							readLockHolderMNapMutex.Lock()
+							delete(readLockHolderMNap[k], transactionID)
+							readLockHolderMNapMutex.Unlock()
+						}
+
+						if v == 2 {
+
+							lockMapMutex.Lock()
+							lockMap[k][2] = 0
+							lockMapMutex.Unlock()
+							writeLockHolderMNapMutex.Lock()
+							writeLockHolderMNap[k] = ""
+							writeLockHolderMNapMutex.Unlock()
+						}
+
+					}
+
+					conn.Write([]byte("COMMIT OK"))
+					breakLoop = true
+
+				case <- abortChans[remoteIpindex]:
+
+					for k, v := range holdLockMap {
+
+						if v == 1 {
+
+							lockMapMutex.Lock()
+							lockMap[k][1] -= 1
+							lockMapMutex.Unlock()
+							readLockHolderMNapMutex.Lock()
+							delete(readLockHolderMNap[k], transactionID)
+							readLockHolderMNapMutex.Unlock()
+						}
+
+						if v == 2 {
+							lockMapMutex.Lock()
+							lockMap[k][2] = 0
+							lockMapMutex.Unlock()
+							writeLockHolderMNapMutex.Lock()
+							writeLockHolderMNap[k] = ""
+							writeLockHolderMNapMutex.Unlock()
+						}
+
+					}
+					fmt.Println("Abort type 1")
+					conn.Write([]byte("ABORTED"))
+					breakLoop = true
+
+				case line := <-setChans[remoteIpindex]:
+					lineSplit := strings.Split(line, " ")
+					server := strings.Split(lineSplit[1],".")[0]
+					abortWait := false
+
+					for {
+
+						lockMapMutex.RLock()
+						_, ok := lockMap[lineSplit[1]]
+						lockMapMutex.RUnlock()
+
+						if !ok {
+
+							lockMapMutex.Lock()
+							lockMap[lineSplit[1]] = map[int]int{}
+							lockMapMutex.Unlock()
+
+						}
+
+						lockMapMutex.RLock()
+						WLock := lockMap[lineSplit[1]][2]
+						RLock := lockMap[lineSplit[1]][1]
+						lockMapMutex.RUnlock()
+
+						if WLock >= 1 {
+
+							if holdLockMap[lineSplit[1]] == 2 {
+								fmt.Println("abortWait = ", abortWait)
+								if abortWait {
+									for k, v := range holdLockMap {
+
+										if v == 1 {
+
+											lockMapMutex.Lock()
+											lockMap[k][1] -= 1
+											lockMapMutex.Unlock()
+											readLockHolderMNapMutex.Lock()
+											delete(readLockHolderMNap[k], transactionID)
+											readLockHolderMNapMutex.Unlock()
+										}
+
+										if v == 2 {
+											lockMapMutex.Lock()
+											lockMap[k][2] = 0
+											lockMapMutex.Unlock()
+											writeLockHolderMNapMutex.Lock()
+											writeLockHolderMNap[k] = ""
+											writeLockHolderMNapMutex.Unlock()
+										}
+
+									}
+									fmt.Println("Abort Type 2")
+									conn.Write([]byte("ABORTED"))
+									breakLoop = true
+									break
+								}
+ 								updateMap[lineSplit[1]] = lineSplit[2]
+								logMap[server] = append(logMap[server], line)
+								conn.Write([]byte("OK"))
+								break
+							} else {
+								select {
+									case <-abortChans[remoteIpindex]:
+										abortWait = true
+									default:
+								}
+							}
+
+						} else {
+
+							if RLock == 0 || ( RLock == 1 && holdLockMap[lineSplit[1]] == 1) {
+								if abortWait {
+									for k, v := range holdLockMap {
+
+										if v == 1 {
+
+											lockMapMutex.Lock()
+											lockMap[k][1] -= 1
+											lockMapMutex.Unlock()
+											readLockHolderMNapMutex.Lock()
+											delete(readLockHolderMNap[k], transactionID)
+											readLockHolderMNapMutex.Unlock()
+										}
+
+										if v == 2 {
+											lockMapMutex.Lock()
+											lockMap[k][2] = 0
+											lockMapMutex.Unlock()
+											writeLockHolderMNapMutex.Lock()
+											writeLockHolderMNap[k] = ""
+											writeLockHolderMNapMutex.Unlock()
+										}
+
+									}
+									fmt.Println("Abort type 3")
+									conn.Write([]byte("ABORTED"))
+									breakLoop = true
+									break
+								}
+
+								lockMapMutex.Lock()
+								lockMap[lineSplit[1]][2] = 1
+								lockMapMutex.Unlock()
+
+								writeLockHolderMNapMutex.Lock()
+								writeLockHolderMNap[lineSplit[1]] = transactionID
+								writeLockHolderMNapMutex.Unlock()
+
+								holdLockMap[lineSplit[1]] = 2
+
+								if RLock == 1 {
+
+									lockMapMutex.Lock()
+									lockMap[lineSplit[1]][1] -= 1
+									readLockHolderMNapMutex.Lock()
+									delete(readLockHolderMNap, lineSplit[1])
+									readLockHolderMNapMutex.Unlock()
+									lockMapMutex.Unlock()
+
+								}
+
+								updateMap[lineSplit[1]] = lineSplit[2]
+								logMap[server] = append(logMap[server], line)
+								conn.Write([]byte("OK"))
+								break
+							} else {
+								select {
+									case <-abortChans[remoteIpindex]:
+										abortWait = true
+									default:
+								}
+							}
+						}
+					}
+
+				case line := <-getChans[remoteIpindex]:
+					abortWait := false
+					lineSplit := strings.Split(line, " ")
+
+					for {
+
+						lockMapMutex.RLock()
+						_, ok := lockMap[lineSplit[1]]
+						lockMapMutex.RUnlock()
+
+						if !ok {
+
+							lockMapMutex.Lock()
+							lockMap[lineSplit[1]] = map[int]int{}
+							lockMapMutex.Unlock()
+
+						}
+
+						lockMapMutex.RLock()
+						WLock := lockMap[lineSplit[1]][2]
+						lockMapMutex.RUnlock()
+
+						if WLock == 1 && holdLockMap[lineSplit[1]] != 2 {
+							select {
+								case <-abortChans[remoteIpindex]:
+									abortWait = true
+								default:
+							}
+							continue
+						}
+
+						if abortWait {
+							for k, v := range holdLockMap {
+
+								if v == 1 {
+									lockMapMutex.Lock()
+									lockMap[k][1] -= 1
+									lockMapMutex.Unlock()
+									readLockHolderMNapMutex.Lock()
+									delete(readLockHolderMNap[k], transactionID)
+									readLockHolderMNapMutex.Unlock()
+								}
+
+								if v == 2 {
+									lockMapMutex.Lock()
+									lockMap[k][2] = 0
+									lockMapMutex.Unlock()
+									writeLockHolderMNapMutex.Lock()
+									writeLockHolderMNap[k] = ""
+									writeLockHolderMNapMutex.Unlock()
+								}
+
+							}
+
+							fmt.Println("Abort type 4")
+							conn.Write([]byte("ABORTED"))
+							breakLoop = true
+							break
+						}
+
+						if holdLockMap[lineSplit[1]] == 0 {
+
+							holdLockMap[lineSplit[1]] = 1
+							lockMapMutex.Lock()
+							lockMap[lineSplit[1]][1] += 1
+
+							readLockHolderMNapMutex.RLock()
+							_, ok := readLockHolderMNap[lineSplit[1]]
+							readLockHolderMNapMutex.RUnlock()
+							readLockHolderMNapMutex.Lock()
+							if ! ok {
+								readLockHolderMNap[lineSplit[1]] = map[string]bool{}
+							}
+							readLockHolderMNap[lineSplit[1]][transactionID] = true
+							readLockHolderMNapMutex.Unlock()
+							lockMapMutex.Unlock()
+						}
+
+						break
+					}
+
+					if(breakLoop) {
+						break
+					}
+
+					fmt.Println("Reached here 2")
+					server := strings.Split(lineSplit[1],".")[0]
+					v, ok := updateMap[lineSplit[1]]
+					fmt.Println("Reached here 3")
+
+					if ok {
+
+						msg := lineSplit[1] + " = " + v
+						conn.Write([]byte(msg))
+
+					} else {
+
+						serverConnMapMutex.RLock()
+						serverConn := serverConnMap[server]
+						serverConnMapMutex.RUnlock()
+						serverConn.Write([]byte(line+"\n"))
+						fmt.Println("Send some bytes to the server")
+						j, err := serverConn.Read(serverBuff)
+
+						if err != nil {
+							fmt.Printf("#Failed to connect to Server%v.", server)
+							panic("Server" + server + " disconnected.")
+						}
+
+						if string(serverBuff[0:j]) == "NO" {
+							// return NOT FOUND and abort the transaction.
+							for k, v := range holdLockMap {
+								if v == 1 {
+
+									lockMapMutex.Lock()
+									lockMap[k][1] -= 1
+									lockMapMutex.Unlock()
+									readLockHolderMNapMutex.Lock()
+									delete(readLockHolderMNap[k], transactionID)
+									readLockHolderMNapMutex.Unlock()
+								}
+
+								if v == 2 {
+									lockMapMutex.Lock()
+									lockMap[k][2] = 0
+									lockMapMutex.Unlock()
+									writeLockHolderMNapMutex.Lock()
+									writeLockHolderMNap[k] = ""
+									writeLockHolderMNapMutex.Unlock()
+								}
+							}
+
+							conn.Write([]byte("NO FOUND"))
+							conn.Write([]byte("This transaction is being aborted, please start a new one."))
+							breakLoop = true
+							break
+
+						} else {
+							fmt.Println("Found in the server")
+							updateMap[lineSplit[1]] = string(serverBuff[0:j])
+							msg := lineSplit[1] + " = " + string(serverBuff[0:j])
+							conn.Write([]byte(msg))
+
+						}
+					}
+
+			}
+		}
+
 	}
+
 }
 
 func main(){
@@ -486,9 +595,11 @@ func main(){
 
 	for i := range abortChans {
 		abortChans[i] = make(chan bool)
+		beginChans[i] = make(chan bool)
+		commitChans[i] = make(chan bool)
+		setChans[i] = make(chan string)
+		getChans[i] = make(chan string)
 	}
-
-	go sendAbortSignal(abortChans[0])
 
 	ip2ChannelIndexMap = make(map[string]int)
 	ip2ChannelIndexMap = map[string]int{
